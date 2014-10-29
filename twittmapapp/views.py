547d6f756993cb5ff7252a06c38c009d347d20eb
Maddy93 <madhavankk@gmail.com>
2014-10-29 02:10:13 -0400

    added map

--- twittmapapp/views.py
+++ twittmapapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-
+from twittmapapp.models import Tweetdata
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the twittmapapp index.")
+    return render(request, 'twittmapapp/index.html')
